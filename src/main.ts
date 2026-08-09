5b05cabb8b5791a9474a8545ccd8c83c486d8f75
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2026-08-09 18:06:24 +0900

    Run Prettier

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,59 @@
-import {
-    Plugin,
-    ItemView,
-    WorkspaceLeaf
-} from 'obsidian';
+import { Plugin, ItemView, WorkspaceLeaf } from "obsidian";
 
 export default class Zincchirp extends Plugin {
-    async onload() {
-        this.registerView(
-            'zincchirp-view',
-            (leaf) => new ZincchirpView(leaf)
-        );
+  async onload() {
+    this.registerView("zincchirp-view", (leaf) => new ZincchirpView(leaf));
 
-        this.addCommand({
-            id: 'open-zincchirp-view',
-            name: 'Open Zincchirp panel',
-            callback: () => {
-                this.activateView();
-            }
-        });
+    this.addCommand({
+      id: "open-zincchirp-view",
+      name: "Open Zincchirp panel",
+      callback: () => {
+        this.activateView();
+      },
+    });
+  }
+
+  onunload() {}
+
+  async activateView() {
+    const { workspace } = this.app;
+
+    let leaf: WorkspaceLeaf | null = null;
+    const leaves = workspace.getLeavesOfType("zincchirp-view");
+
+    if (leaves.length > 0) {
+      // A leaf with our view already exists, use that
+      leaf = leaves[0] || null;
+    } else {
+      // Our view could not be found in the workspace, create a new leaf
+      // in the right sidebar for it
+      leaf = workspace.getRightLeaf(false);
+      await leaf?.setViewState({ type: "zincchirp-view", active: true });
     }
 
-    onunload() {
+    // "Reveal" the leaf in case it is in a collapsed sidebar
+    if (leaf) {
+      workspace.revealLeaf(leaf);
     }
-
-
-    async activateView() {
-        const { workspace } = this.app;
-
-        let leaf: WorkspaceLeaf | null = null;
-        const leaves = workspace.getLeavesOfType('zincchirp-view');
-
-        if (leaves.length > 0) {
-            // A leaf with our view already exists, use that
-            leaf = leaves[0] || null;
-        } else {
-            // Our view could not be found in the workspace, create a new leaf
-            // in the right sidebar for it
-            leaf = workspace.getRightLeaf(false);
-            await leaf?.setViewState({ type: 'zincchirp-view', active: true });
-        }
-
-        // "Reveal" the leaf in case it is in a collapsed sidebar
-        if (leaf) {
-            workspace.revealLeaf(leaf);
-        }
-    }
+  }
 }
 
 export class ZincchirpView extends ItemView {
-    constructor(leaf: WorkspaceLeaf) {
-        super(leaf);
-    }
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
+  }
 
-    getViewType() {
-        return 'zincchirp-view';
-    }
+  getViewType() {
+    return "zincchirp-view";
+  }
 
+  getDisplayText() {
+    return "Hello World";
+  }
 
-    getDisplayText() {
-        return 'Hello World';
-    }
-
-    async onOpen() {
-        const container = this.contentEl;
-        container.empty();
-        container.createEl('h4', { text: 'Hello World' });
-    }
+  async onOpen() {
+    const container = this.contentEl;
+    container.empty();
+    container.createEl("h4", { text: "Hello World" });
+  }
 }
